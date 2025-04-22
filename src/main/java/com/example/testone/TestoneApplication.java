61f1142735b1f14fd1602292ec8ba9d89e398ad2
Brintharun03 <kbrintha0304@gmail.com>
2025-04-22 07:38:48 +0530

    my second commit hai

--- src/main/java/com/example/testone/TestoneApplication.java
+++ src/main/java/com/example/testone/TestoneApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestoneApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(TestoneApplication.class, args);
+        System.out.println("A");
     }
 
 }
